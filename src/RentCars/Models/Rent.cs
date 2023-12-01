aad32ea8e32318a7eb651d27180d712c046b8f53
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-12-01 07:25:07 -0300

    req 9 e 10

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -13,7 +13,40 @@ public class Rent
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        throw new NotImplementedException();
+        Vehicle = vehicle;
+        Person = person;
+
+        if (daysRented <= 0)
+        {
+            throw new ArgumentException("O número de dias alugados deve ser maior que zero.");
+        }
+
+        DaysRented = daysRented;
+
+        CalculatePrice();
+        Status = RentStatus.Confirmed;
+
+        vehicle.IsRented = true;
+
+        // Altera o atributo Debit da pessoa para o preço calculado
+        person.Debit += Price;
+    }
+
+    private void CalculatePrice()
+    {
+        // calcular o preço
+        double dailyRate = Vehicle.PricePerDay;
+
+        if (Person is LegalPerson)
+        {
+            // Pessoas jurídicas têm desconto de 10%
+            Price = dailyRate * DaysRented * 0.9;
+        }
+        else
+        {
+            // Pessoas físicas
+            Price = dailyRate * DaysRented;
+        }
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
